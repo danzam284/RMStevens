ab3b4f6148d1137879e485be2496a3046b52e1ee
danzam284 <45713834+danzam284@users.noreply.github.com>
2024-12-03 09:33:35 -0500

    Added admin capabilities, added seed, bug fixes

--- helpers.js
+++ helpers.js
@@ -2,17 +2,27 @@ import validator from 'email-validator';
 
 function validateString(str) {
     if (!str || typeof str !== 'string') {
-        throw Error("Value is not a string.");
+        throw "Value is not a string.";
     }
     if (str.trim() == "") {
-        throw Error("String is either empty or only has spaces.");
+        throw "String is either empty or only has spaces.";
     }
 }
 
 function validateEmail(str) {
     validateString(str);
     if (!validator.validate(str)) {
-        throw Error("Invalid email.");
+        throw "Invalid email.";
+    }
+}
+
+function validateEmailStevens(str) {
+    validateString(str);
+    if (!validator.validate(str)) {
+        throw "Invalid email.";
+    }
+    if (!str.endsWith('@stevens.edu')) {
+        throw "Email must be a Stevens Email.";
     }
 }
 
@@ -21,11 +31,11 @@ function validateName(n) {
     validateString(n);
 
     if (n.length < 2 || n.length > 25) {
-        throw Error("Invalid name length");
+        throw "Invalid name length";
     }
     for (let i = 0; i < n; i++) {
         if (!validCharacters.includes(n[i]) && !validCharacters.toUpperCase().includes(n[i])) {
-            throw Error("Name is not valid.");
+            throw "Name is not valid.";
         }
     }
 }
@@ -37,12 +47,12 @@ const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 function validatePassword(p) {
     validateString(p);
     if (p.length < 8) {
-        throw Error("Password must be longer.");
+        throw "Password must be longer.";
     }
     let spe = false, num = false, upp = false;
     for (let i = 0; i < p.length; i++) {
         if (p[i] === " ") {
-            throw Error("Password must not contain spaces.");
+            throw "Password must not contain spaces.";
         }
         if (specialCharacters.includes(p[i])) {
             spe = true;
@@ -53,17 +63,17 @@ function validatePassword(p) {
         }
     }
     if (!spe || !num || !upp) {
-        throw Error("Password does not meet requirements.");
+        throw "Password does not meet requirements.";
     }
 }
 
 function validateUsername(u) {
     validateString(u);
     if (u.length < 3) {
-        throw Error("Username must be longer.");
+        throw "Username must be longer.";
     }
     if (u.length > 15) {
-        throw Error("Username is too long.");
+        throw "Username is too long.";
     }
 }
 
@@ -76,41 +86,42 @@ function validateCourseName(n) {
     validateString(n);
     const spl = n.split(" ");
     if (spl.length !== 2) {
-        throw Error("Invalid course name format.");
+        throw "Invalid course name format.";
     }
     if (!validPrefixes.includes(spl[0].trim().toUpperCase())) {
-        throw Error("Invalid course prefix.");
+        throw "Invalid course prefix.";
     }
     if (isNaN(spl[1].trim())) {
-        throw Error("Non-number detected for course number.");
+        throw "Non-number detected for course number.";
     }
     const courseNumber = parseInt(spl[1].trim());
     if (courseNumber < 100 || courseNumber > 900) {
-        throw Error("Course number must be between 100 and 900.");
+        throw "Course number must be between 100 and 900.";
     }
 }
 
 function validateProfessorName(n, type) {
     validateString(n);
     if (n.length < 2) {
-        throw Error(type + " name is too short.");
+        throw type + " name is too short.";
     }
     if (n.length > 25) {
-        throw Error(type + " name is too long.");
+        throw type + " name is too long.";
     }
     for (let i = 0; i < n.length; i++) {
         if (!(/[a-zA-Z]/).test(n[i]) && n[i] !== "'") {
-            throw Error(type + " name has an invalid character.");
+            throw type + " name has an invalid character.";
         }
     }
     if (n.split("'").length > 2) {
-        throw Error("Too many apostrophes in " + type + " name.")
+        throw "Too many apostrophes in " + type + " name.";
     }
 }
 
 export {
     validateString,
     validateEmail,
+    validateEmailStevens,
     validateName,
     validatePassword,
     validateUsername,
--- public/js/client_side_validation.js
+++ public/js/client_side_validation.js
@@ -1,12 +1,12 @@
-
 document.addEventListener('DOMContentLoaded', function () {
     let registerForm = document.getElementById('registration-form');
     let loginForm = document.getElementById('login-form');
+    let createForm = document.getElementById('create-form');
+    let courseForm = document.getElementById('course-form');
+    let professorForm = document.getElementById('professor-form');
 
     if (registerForm) {
-    
       registerForm.addEventListener('submit', function (event) {
-
         event.preventDefault();
 
         const emailAddressInput = document.getElementById('emailAddressInput').value;
@@ -15,24 +15,32 @@ document.addEventListener('DOMContentLoaded', function () {
       
         // email input validation
         let validEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)+$/;
+        if (!validEmail.test(emailAddressInput) || !emailAddressInput.includes('@stevens.edu')) { //check for all the valid email stuff above
+            document.getElementById("error").innerHTML = "Please enter a valid Stevens email address (Must contain @stevens.edu).";
+            return;
+        }
 
-        if (!validEmail.test(emailAddressInput)) { //check for all the valid email stuff above
-            throw new Error ("please enter a valid email address")
+        if (!emailAddressInput.endsWith('@stevens.edu')) {
+          document.getElementById("error").innerHTML = "Email must be a Stevens Email.";
+          return;
         }
 
         // password input validation
         let validPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]).{8,}$/;
 
         if (!validPassword.test(passwordInput)) {
-            throw new Error('please enter valid password');
+          document.getElementById("error").innerHTML = "Please enter a valid password";
+          return;
         }
 
         if (!validPassword.test(confirmPasswordInput)) {
-            throw new Error('please enter valid password');
+          document.getElementById("error").innerHTML = "Please enter a valid password";
+          return;
         }
 
         if (passwordInput !== confirmPasswordInput) {
-            return new Error ('passwords must match');
+          document.getElementById("error").innerHTML = "Passwords must match";
+          return;
         }
 
         registerForm.submit();
@@ -42,31 +50,211 @@ document.addEventListener('DOMContentLoaded', function () {
   
     if (loginForm) {
       loginForm.addEventListener('submit', function (event) {
-        
         event.preventDefault();
 
         let emailAddressInput = document.getElementById('emailAddressInput').value;
         let passwordInput = document.getElementById('passwordInput').value;
-
         
         // email input validation
         let validEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)+$/;
-
-        if (!validEmail.test(emailAddressInput)) { //check for all the valid email stuff above
-            throw new Error ("please enter a valid email address")
+        
+        if (!validEmail.test(emailAddressInput) || !emailAddressInput.includes('@stevens.edu')) { //check for all the valid email stuff above
+            document.getElementById("error").innerHTML = "Please enter a valid Stevens Email Address (Must contain @stevens.edu).";
+            return;
         }
 
         // password input validation
         let validPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]).{8,}$/;
 
         if (!validPassword.test(passwordInput)) {
-            throw new Error('please enter valid password');
+          document.getElementById("error").innerHTML = "Please enter a valid password.";
+          return;
         }
-
         loginForm.submit();
         
       });
     }
-  
+
+    if (createForm) {
+      createForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        let courseNameInput = document.getElementById('courseNameInput').value;
+        let reviewInput = document.getElementById('reviewTextInput').value;
+        let ratingInput = document.getElementById('ratingInput').value;
+        let difficultyInput = document.getElementById('difficultyInput').value;
+        let professorNameInput = document.getElementById('professorNameInput').value;
+
+        courseNameInput = courseNameInput.trim();
+        if (typeof courseNameInput !== 'string' || courseNameInput.trim().length === 0 || !courseNameInput) {
+          document.getElementById("error").innerHTML = "Please enter a valid course name.";
+          return;
+        }
+
+        if (isNaN(parseInt(ratingInput)) || parseInt(ratingInput) < 1 || parseInt(ratingInput) > 5) {
+          document.getElementById("error").innerHTML = "Please enter a valid rating.";
+          return;
+        } 
+
+        if (isNaN(parseInt(difficultyInput)) || parseInt(difficultyInput) < 1 || parseInt(difficultyInput) > 5) {
+          document.getElementById("error").innerHTML = "Please enter a valid difficulty.";
+          return;
+        }
+
+        reviewInput = reviewInput.trim();
+        if (typeof reviewInput !== 'string' || reviewInput.trim().length === 0) {
+          document.getElementById("error").innerHTML = "Please enter a valid review.";
+          return;
+        }
+
+        professorNameInput = professorNameInput.trim();
+        if (typeof professorNameInput !== 'string' || professorNameInput.length === 0 || !professorNameInput) {
+          document.getElementById("error").innerHTML = "Please enter a valid professor name.";
+          return;
+        }
+        createForm.submit();
+      });
+    }
+
+    if (courseForm) {
+      courseForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        let courseNameInput = document.getElementById('courseNameInput').value;
+        const validPrefixes = ["AAI", "ACC", "BIA", "BIO", "BME", "BT", "CAL", "CE", "CH", "CHE", "CM", "CPE", "CS", "DS", "ECON", "EE", "ELC", "EM", "EN", "ENGR", "FA", "FE", "FIN", "HAR", "HMU", "HSS", "HST", "IDE", "ISE", "MA", "ME", "MGT", "MIS", "MT", "NANO", "NE", "NIS", "OE", "PEP"];
+        courseNameInput = courseNameInput.trim();
+
+        if (typeof courseNameInput !== 'string' || courseNameInput.trim().length === 0 || !courseNameInput) {
+          document.getElementById("error").innerHTML = "Please enter a valid course name.";
+          return;
+        }
+
+        const spl = courseNameInput.split(" ");
+        if (spl.length !== 2) {
+            document.getElementById("error").innerHTML = "Invalid course name format.";
+            return;
+        }
+        if (!validPrefixes.includes(spl[0].trim().toUpperCase())) {
+            document.getElementById("error").innerHTML = "Invalid course prefix.";
+            return;
+        }
+        if (isNaN(spl[1].trim())) {
+            document.getElementById("error").innerHTML = "Non-number detected for course number.";
+            return;
+        }
+        const courseNumber = parseInt(spl[1].trim());
+        if (courseNumber < 100 || courseNumber > 900) {
+            document.getElementById("error").innerHTML = "Course number must be between 100 and 900.";
+            return;
+        }
+
+        courseForm.submit();
+      });
+    }
+
+    if (professorForm) {
+      professorForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        let professorFirstNameInput = document.getElementById('professorFirstNameInput').value;
+        let professorLastNameInput = document.getElementById('professorLastNameInput').value;
+        professorFirstNameInput = professorFirstNameInput.trim();
+        professorLastNameInput = professorLastNameInput.trim();
+
+
+        if (typeof professorFirstNameInput !== 'string' || professorFirstNameInput.length === 0 || !professorFirstNameInput || professorFirstNameInput.indexOf(" ") >= 0) {
+          document.getElementById("error").innerHTML = "Please enter a valid first name for the professor.";
+          return;
+        }
+
+        if (typeof professorLastNameInput !== 'string' || professorLastNameInput.length === 0 || !professorLastNameInput || professorLastNameInput.indexOf(" ") >= 0) {
+          document.getElementById("error").innerHTML = "Please enter a valid last name for the professor.";
+          return;
+        }
+
+        if (professorFirstNameInput.length < 2 || professorFirstNameInput.length > 25) {
+          document.getElementById("error").innerHTML = "First name needs to be between 2-25 charcters long.";
+          return;
+        }
+
+        if (professorLastNameInput.length < 2 || professorLastNameInput.length > 25) {
+          document.getElementById("error").innerHTML = "Last name needs to be between 2-25 charcters long.";
+          return;
+        }
+
+        for (let i = 0; i < professorFirstNameInput.length; i++) {
+          if (!(/[a-zA-Z]/).test(professorFirstNameInput[i]) && professorFirstNameInput[i] !== "'") {
+              document.getElementById("error").innerHTML = "First name has an invalid character.";
+              return;
+          }
+        }
+
+        for (let i = 0; i < professorLastNameInput.length; i++) {
+          if (!(/[a-zA-Z]/).test(professorLastNameInput[i]) && professorLastNameInput[i] !== "'") {
+              document.getElementById("error").innerHTML = "Last name has an invalid character.";
+              return;
+          }
+        }
+
+        if (professorFirstNameInput.split("'").length > 2) {
+          document.getElementById("error").innerHTML = "Too many apostrophes for the inputted first name.";
+          return;
+        }
+
+        if (professorLastNameInput.split("'").length > 2) {
+          document.getElementById("error").innerHTML = "Too many apostrophes for the inputted entered last name.";
+          return;
+        }
+
+        professorForm.submit();
+      });
+    }
 
 });
+
+(function ($) {
+  let form = $('#professorExists')
+  if (form) {
+    form.submit(function (event) {
+      event.preventDefault();
+      const profName = document.getElementById("professorName").value.trim();
+      const split = profName.split(" ");
+      const error = $("#error");
+      const response = $("#existResponse");
+      response.empty();
+      error.empty();
+      if (split.length !== 2) {
+        error.html("Professor name is not valid");
+        return;
+      }
+      let firstName = split[0], lastName = split[1];
+      if (firstName.length < 2 || firstName.length > 25) {
+        error.html("First name is not valid");
+        return;
+      }
+      if (lastName.length < 2 || lastName.length > 25) {
+        error.html("Last name is not valid");
+        return;
+      }
+      firstName = firstName[0].toUpperCase() + firstName.substring(1);
+      lastName = lastName[0].toUpperCase() + lastName.substring(1);
+
+      $.ajax({
+        method: 'GET',
+        url: '/checkProfessor/' + firstName + " " + lastName,
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        contentType: 'application/json',
+        data: {},
+        success: function(data) {
+          console.log(data);
+          if (data.error) {
+            error.html(data.error);
+          } else if (data.name) {
+            response.html("Yes, " + firstName + " " + lastName + " is in our database! So far " + data.reviewIds.length + " person(s) have rated them and they have an average rating of " + data.averageRating + ".");
+          } else {
+            response.html("No, " + firstName + " " + lastName + " is not in our database. Feel free to go to 'Add Professor' to add them!");
+          }
+        }
+      })
+    });
+  }
+})(window.jQuery);
